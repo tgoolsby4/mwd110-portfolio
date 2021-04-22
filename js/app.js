25c97a14766a925ec678961993a72ea8499a771c
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-22 16:47:34 -0400

    Add button that translates div on page 3

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ window.onload = () => {
     alert(`Hello!`);
 
     let buttonOne = document.querySelector(`#button1`);
+    let buttonPageThree = document.querySelector(`#thirdPageButton`);
 
     let changeColor = () => {
         let background = document.querySelector(`body`);
@@ -9,5 +10,11 @@ window.onload = () => {
         background.style.color = `white`;
     }
 
+    let transforms = () => {
+        let box = document.querySelector(`#transform`);
+        box.style.transform = `translate(` + 50 + `px,` + 100 + `px)`;
+    }
+
+    buttonPageThree.addEventListener(`click`, transforms);
     buttonOne.addEventListener(`click`, changeColor);
 }
