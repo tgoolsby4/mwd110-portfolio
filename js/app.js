29c92fb3c58ab737e94d09b778f8aa041cabf5c5
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-22 16:26:38 -0400

    Create function for button on second page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 window.onload = () => {
     alert(`Hello!`);
+
+    let buttonOne = document.querySelector(`#button1`);
+
+    let changeColor = () => {
+        let background = document.querySelector(`body`);
+        background.style.backgroundColor = `black`;
+        background.style.color = `white`;
+    }
+
+    buttonOne.addEventListener(`click`, changeColor);
 }
