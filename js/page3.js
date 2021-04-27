ff1eaee3ba6724f2f5ec286bf1c7e1fa5b74b4d1
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-27 16:01:10 -0400

    Update transform function in page3.js

--- js/page3.js
+++ js/page3.js
@@ -1,11 +1,21 @@
 window.onload = () => {
     
     let buttonPageThree = document.querySelector(`#thirdPageButton`);
+    let translated = false;
 
     let transforms = () => {
         let box = document.querySelector(`#transform`);
-        box.style.transform = `translate(` + 50 + `px,` + 100 + `px)`;
-    }
+        if(!translated) {
+            box.style.transition = `transform 2s`;
+            box.style.transform = `translate(` + 50 + `px,` + 100 + `px)`;
+            translated = true;
+        }
+        else {
+            box.style.transition = `transform 2s`;
+            box.style.transform = `translate(` + 0 + `px,` + 0 + `px)`;
+            translated = false;
+        }
+    };
 
     buttonPageThree.addEventListener(`click`, transforms);
 
