b0da55bc5d23148bce9d3e83d496f2aa55354315
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-29 16:35:57 -0400

    Allow button to change colors both ways

--- js/page2.js
+++ js/page2.js
@@ -2,11 +2,21 @@ window.onload = () => {
     alert(`Hello!`);
 
     let buttonOne = document.querySelector(`#button1`);
+    let inverted = false;
 
     let changeColor = () => {
-        let background = document.querySelector(`body`);
-        background.style.backgroundColor = `black`;
-        background.style.color = `white`;
+        if(!inverted) {
+            let background = document.querySelector(`body`);
+            background.style.backgroundColor = `black`;
+            background.style.color = `white`;
+            inverted = true;
+        }
+        else {
+            let background = document.querySelector(`body`);
+            background.style.backgroundColor = `hsl(200, 80%, 75%)`;
+            background.style.color = `black`;
+            inverted = false;
+        }
     }
 
     buttonOne.addEventListener(`click`, changeColor);
