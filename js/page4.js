d27aa5964eb0ff60ee51cd1783ba3c3bdbf1b3a0
tgoolsby4 <tgoolsby423@gmail.com>
2021-05-06 16:43:04 -0400

    Make videos appear by an offset of .2 seconds

--- js/page4.js
+++ js/page4.js
@@ -1,14 +1,29 @@
 window.onload = () => {
 
-    let videos = document.querySelectorAll(`.video`);
-    
-    let appear = (x) => {
-        x.style.visibility = `visible`;
-    };
+    let videos = [];
+    videos[0] = document.querySelector(`#v1`);
+    videos[1] = document.querySelector(`#v2`);
+    videos[2] = document.querySelector(`#v3`);
+    videos[3] = document.querySelector(`#v4`);
+    videos[4] = document.querySelector(`#v5`);
+    videos[5] = document.querySelector(`#v6`);
 
-    for(i=0; i<videos.length; i++){
-        let delay = 1000;
-        setTimeout(appear(videos[i]), delay);
-        delay += 1000;
-    }
+    setTimeout(function(){
+        videos[0].style.visibility = `visible`;
+        setTimeout(function(){
+            videos[1].style.visibility = `visible`;
+            setTimeout(function(){
+                videos[2].style.visibility = `visible`;
+                setTimeout(function(){
+                    videos[3].style.visibility = `visible`;
+                    setTimeout(function(){
+                        videos[4].style.visibility = `visible`;
+                        setTimeout(function() {
+                            videos[5].style.visibility = `visible`;
+                        }, 200);
+                    }, 200);
+                }, 200);
+            }, 200);
+        }, 200);
+    }, 200);
 }
