5b24bce8a13561229deff73076a42bf026c75012
tgoolsby4 <tgoolsby423@gmail.com>
2021-05-09 19:29:51 -0400

    Create aquarium page

--- js/page3.js
+++ js/page3.js
@@ -1,20 +1,16 @@
 window.onload = () => {
     
     let buttonPageThree = document.querySelector(`#thirdPageButton`);
-    let translated = false;
 
     let transforms = () => {
         let box = document.querySelector(`#transform`);
-        if(!translated) {
-            box.style.transition = `transform 2s`;
-            box.style.transform = `translate(` + 50 + `px,` + 100 + `px)`;
-            translated = true;
-        }
-        else {
-            box.style.transition = `transform 2s`;
-            box.style.transform = `translate(` + 0 + `px,` + 0 + `px)`;
-            translated = false;
-        }
+        let tiny = document.querySelector(`#tiny`);
+        let left = Math.floor(Math.random()*(1335-10+1)+10);
+        let top = Math.floor(Math.random()*(400-10+1)+10);
+        tiny.style.left = left+`px`;
+        tiny.style.top = top+`px`;
+        box.style.transition = `transform 2s`;
+        box.style.transform = `translate(` + (left-100) + `px,` + top + `px)`;
     };
 
     buttonPageThree.addEventListener(`click`, transforms);
